6d462f58c924f8cbfc9c3d89605f7775adbb5ef1
Paul Verheul <paul@nilsenpaul.nl>
2015-01-09 13:02:50 +0100

    Fixed bug that stopped this plugin from working after 2.3.2624

--- resources/addattributes.js
+++ resources/addattributes.js
@@ -1,30 +1,34 @@
-/*
-[{"btnName":"Inline Code","className":"redactor_format_pre","wrap":"code","forceBlock":"0"}]
- */
-
 if (!RedactorPlugins) var RedactorPlugins = {};
 
-RedactorPlugins.addattributes = {
-	init: function()
-	{
-		try { this.opts.addAttributes = jQuery.parseJSON(this.opts.addAttributes); } catch(e) {} 
+(function($)
+{
+    RedactorPlugins.addattributes = function()
+    {
+        return {
+            init: function()
+            {
+                try { this.opts.addAttributes = jQuery.parseJSON(this.opts.addAttributes); } catch(e) {}
 
-		this.opts.changeCallback = function() { this.addAttributes(); }
-	},
-	addAttributes: function()
-	{
-		var editor = this.$editor;
-		jQuery.each(this.opts.addAttributes, function(i, data) {
-			for(var tag in data) {
-				editor.find(tag).each(function(i, elem) {
-					jQuery.each(data[tag], function(i, tagData) {
-						for(var attr in tagData) {
-							$(elem).prop(attr, tagData[attr]);
-						}
-					});
-				});
-			}
-		});
-		this.sync();
-	},
-};
+                this.opts.changeCallback = function() { this.addattributes.addAttributes(); }
+
+            },
+            addAttributes: function()
+            {
+                var editor = this.$editor;
+
+                jQuery.each(this.opts.addAttributes, function(i, data) {
+                    for(var tag in data) {
+                        editor.find(tag).each(function(i, elem) {
+                            jQuery.each(data[tag], function(i, tagData) {
+                                for(var attr in tagData) {
+                                    $(elem).prop(attr, tagData[attr]);
+                                }
+                            });
+                        });
+                    }
+                });
+                this.code.sync();
+            }
+        };
+    };
+})(jQuery);
